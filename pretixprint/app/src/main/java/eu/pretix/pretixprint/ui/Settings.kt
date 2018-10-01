4404bcfa1df2c952426176d27a81348b45d81b47
Raphael Michel <mail@raphaelmichel.de>
2018-10-01 17:18:48 +0200

    Add license dialog

--- pretixprint/app/src/main/java/eu/pretix/pretixprint/ui/Settings.kt
+++ pretixprint/app/src/main/java/eu/pretix/pretixprint/ui/Settings.kt
@@ -4,9 +4,20 @@ package eu.pretix.pretixprint.ui
 import android.content.Intent
 import android.os.Bundle
 import android.preference.PreferenceFragment
+import android.text.Html
 import android.text.TextUtils
+import android.text.method.LinkMovementMethod
+import android.view.LayoutInflater
+import android.widget.TextView
+import androidx.annotation.RawRes
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
 import eu.pretix.pretixprint.R
 import org.jetbrains.anko.defaultSharedPreferences
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
 
 
 class SettingsFragment : PreferenceFragment() {
@@ -26,6 +37,10 @@ class SettingsFragment : PreferenceFragment() {
                 return@setOnPreferenceClickListener true
             }
         }
+        findPreference("licenses").setOnPreferenceClickListener {
+            asset_dialog(R.raw.about, R.string.settings_label_licenses)
+            return@setOnPreferenceClickListener true
+        }
     }
 
     override fun onResume() {
@@ -42,6 +57,47 @@ class SettingsFragment : PreferenceFragment() {
             }
         }
     }
+
+
+    private fun asset_dialog(@RawRes htmlRes: Int, @StringRes title: Int) {
+        val view = LayoutInflater.from(activity).inflate(R.layout.dialog_about, null, false)
+        val dialog = AlertDialog.Builder(activity)
+                .setTitle(title)
+                .setView(view)
+                .setPositiveButton(R.string.dismiss, null)
+                .create()
+
+        val textView = view.findViewById(R.id.aboutText) as TextView
+
+        var text = ""
+
+        val builder = StringBuilder()
+        val fis: InputStream
+        try {
+            fis = resources.openRawResource(htmlRes)
+            val reader = BufferedReader(InputStreamReader(fis, "utf-8"))
+            while (true) {
+                val line = reader.readLine()
+                if (line != null) {
+                    builder.append(line)
+                } else {
+                    break
+                }
+            }
+
+            text = builder.toString()
+            fis.close()
+        } catch (e: IOException) {
+            //Sentry.captureException(e)
+            e.printStackTrace()
+        }
+
+        textView.text = Html.fromHtml(text)
+        textView.movementMethod = LinkMovementMethod.getInstance()
+
+        dialog.show()
+    }
+
 }
 
 class SettingsActivity : AppCompatPreferenceActivity() {
