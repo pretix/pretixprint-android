74577b2312bfd52fe3f2a5733c5e4570ad84faa0
Raphael Michel <mail@raphaelmichel.de>
2019-03-16 19:28:41 +0100

    FGL printing: Do not sent CB, send pixels in smaller chunks

--- pretixprint/app/src/main/java/eu/pretix/pretixprint/socket/FGLNetworkPrinter.kt
+++ pretixprint/app/src/main/java/eu/pretix/pretixprint/socket/FGLNetworkPrinter.kt
@@ -10,27 +10,36 @@ class FGLNetworkPrinter(ip: String, port: Int, dpi: Int) : SocketNetworkPrinter(
 
     override fun convertPageToBytes(img: Bitmap): ByteArray {
         val ostream = ByteArrayOutputStream()
-        val pixels = IntArray(img.width * img.height)
-        img.getPixels(pixels, 0, img.width, 0, 0, img.width, img.height)
+        val w = img.width
+        val h = img.height
+        val stepsize = 100
+        val pixels = IntArray(w * h)
+        img.getPixels(pixels, 0, w, 0, 0, w, h)
 
-        ostream.write("<CB>".toByteArray())
-        for (yoffset in 0..(img.height - 1) step 8) {
-            val row = ByteArray(img.width)
-            for (x in 0..(img.width - 1)) {
-                var col = 0
-                for (j in 0..7) {
-                    val px = pixels[min(x + img.width * (yoffset + j), pixels.size - 1)]
-                    if ((px shr 24) and 0xff > 128 && ((px shr 16) and 0xff < 128 || (px shr 8) and 0xff < 128 || px and 0xff < 128)) {
-                        // A > 128 && (R < 128 || G < 128 || B < 128)
-                        col = col or (1 shl (7 - j))
+        //ostream.write("<CB>".toByteArray())
+        for (yoffset in 0..(h - 1) step 8) {
+            for (xoffset in 0..(w - 1) step stepsize) {
+                val row = ByteArray(stepsize)
+                var any = false
+                for (x in xoffset..min(xoffset + stepsize - 1, w - 1)) {
+                    var col = 0
+                    for (j in 0..7) {
+                        val px = pixels[min(x + w * (yoffset + j), pixels.size - 1)]
+                        if ((px shr 24) and 0xff > 128 && ((px shr 16) and 0xff < 128 || (px shr 8) and 0xff < 128 || px and 0xff < 128)) {
+                            // A > 128 && (R < 128 || G < 128 || B < 128)
+                            col = col or (1 shl (7 - j))
+                        }
                     }
+                    row[x - xoffset] = col.toByte()
+                    if (col > 0)
+                        any = true
                 }
-                row[x] = col.toByte()
+                if (any) {
+                    ostream.write("<RC${yoffset},${xoffset}><G${stepsize}>".toByteArray())
+                    ostream.write(row)
+                    ostream.write("\n".toByteArray())
+                }
             }
-            ostream.write("<RC${yoffset},0><G${img.width}>".toByteArray())
-            ostream.write(row)
-            ostream.write("\n".toByteArray())
-            ostream.flush()
         }
         ostream.write("<p>\n".toByteArray())
         return ostream.toByteArray()
