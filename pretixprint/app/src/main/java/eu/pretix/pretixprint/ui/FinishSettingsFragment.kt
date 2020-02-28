c35679bdb8c4c219e1c5acdf1c8ea71fa28913bf
Raphael Michel <mail@raphaelmichel.de>
2020-02-28 15:30:55 +0100

    UX feedback for test printing

--- pretixprint/app/src/main/java/eu/pretix/pretixprint/ui/FinishSettingsFragment.kt
+++ pretixprint/app/src/main/java/eu/pretix/pretixprint/ui/FinishSettingsFragment.kt
@@ -12,8 +12,13 @@ import eu.pretix.pretixprint.connections.BluetoothConnection
 import eu.pretix.pretixprint.connections.CUPSConnection
 import eu.pretix.pretixprint.connections.NetworkConnection
 import eu.pretix.pretixprint.connections.USBConnection
+import kotlinx.coroutines.experimental.withTimeout
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.support.v4.alert
 import org.jetbrains.anko.support.v4.ctx
+import org.jetbrains.anko.support.v4.indeterminateProgressDialog
+import org.jetbrains.anko.support.v4.progressDialog
+import org.jetbrains.anko.uiThread
 import java.io.File
 import java.io.FileOutputStream
 
@@ -60,44 +65,36 @@ class FinishSettingsFragment : SetupFragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(R.layout.fragment_finish_settings, container, false)
         val activity = activity as PrinterSetupActivity
+        val view = inflater.inflate(R.layout.fragment_finish_settings, container, false)
 
         view.findViewById<Button>(R.id.btnTestPage).setOnClickListener {
-            val file = when (activity.useCase) {
-                "receipt" -> writeDemoTxt()
-                else -> writeDemoPdf()
-            }
-            when (activity.mode()) {
-                NetworkConnection().identifier -> {
-                    doAsync {
-                        NetworkConnection().print(file, 1, activity!!, activity.useCase, activity.settingsStagingArea)
+            val pb = indeterminateProgressDialog(R.string.testing)
+            pb.setCancelable(true)
+            pb.show()
+            doAsync {
+                try {
+                    testprint()
+                    uiThread {
+                        if (this@FinishSettingsFragment.activity == null)
+                            return@uiThread
+                        alert(R.string.test_success).show()
+                    }
+                } catch (e: PrintException) {
+                    uiThread {
+                        if (this@FinishSettingsFragment.activity == null)
+                            return@uiThread
+                        alert(e.message).show()
+                    }
+                } finally {
+                    uiThread {
+                        if (this@FinishSettingsFragment.activity == null)
+                            return@uiThread
+                        pb.dismiss()
                     }
                 }
-                BluetoothConnection().identifier -> {
-                    doAsync {
-                        BluetoothConnection().print(file, 1, activity!!, activity.useCase, activity.settingsStagingArea)
-                    }
-                }
-                CUPSConnection().identifier -> {
-                    doAsync {
-                        CUPSConnection().print(file, 1, activity!!, activity.useCase, activity.settingsStagingArea)
-                    }
-                }
-                USBConnection().identifier -> {
-                    doAsync {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                            USBConnection().print(file, 1, activity!!, activity.useCase, activity.settingsStagingArea)
-                        } else {
-                            throw Exception("USB not supported on this Android version.")
-                        }
-                    }
-                }
-            }
 
-            // TODO: Better decision, what kind of demopage we're sending
-            // TODO: progress bar
-            // TODO: other connections
+            }
         }
         view.findViewById<Button>(R.id.btnPrev).setOnClickListener {
             back()
@@ -112,5 +109,31 @@ class FinishSettingsFragment : SetupFragment() {
 
     override fun back() {
         (activity as PrinterSetupActivity).startProtocolSettings(true)
+    }
+
+    fun testprint() {
+        val activity = activity as PrinterSetupActivity
+        val file = when (activity.useCase) {
+            "receipt" -> writeDemoTxt()
+            else -> writeDemoPdf()
+        }
+        when (activity.mode()) {
+            NetworkConnection().identifier -> {
+                NetworkConnection().print(file, 1, activity!!, activity.useCase, activity.settingsStagingArea)
+            }
+            BluetoothConnection().identifier -> {
+                BluetoothConnection().print(file, 1, activity!!, activity.useCase, activity.settingsStagingArea)
+            }
+            CUPSConnection().identifier -> {
+                CUPSConnection().print(file, 1, activity!!, activity.useCase, activity.settingsStagingArea)
+            }
+            USBConnection().identifier -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    USBConnection().print(file, 1, activity!!, activity.useCase, activity.settingsStagingArea)
+                } else {
+                    throw Exception("USB not supported on this Android version.")
+                }
+            }
+        }
     }
 }
