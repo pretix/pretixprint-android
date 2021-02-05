708ec2252753cda15a7200a7d729070e6e17a643
Raphael Michel <mail@raphaelmichel.de>
2021-02-05 11:45:33 +0100

    Add USB device filters

---
+++ pretixprint/app/src/main/java/eu/pretix/pretixprint/ui/USBAttachedActivity.kt
@@ -0,0 +1,11 @@
+package eu.pretix.pretixprint.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class USBAttachedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish()
+    }
+}
