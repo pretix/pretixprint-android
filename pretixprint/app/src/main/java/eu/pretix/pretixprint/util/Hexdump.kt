343d1af6f5485ec66279e00457ed71e63ea3ad73
Maximilian Richt <richt@rami.io>
2022-05-18 15:27:40 +0200

    Maintenance: set hexdump width to 8 byte

--- pretixprint/app/src/main/java/eu/pretix/pretixprint/util/Hexdump.kt
+++ pretixprint/app/src/main/java/eu/pretix/pretixprint/util/Hexdump.kt
@@ -1,32 +1,33 @@
 package eu.pretix.pretixprint.util
 
 
-class HexdumpByteArrayHolder {
-    companion object {
-        const val MAX_ITEMS = 16
-    }
-    private val bytes: ByteArray = ByteArray(MAX_ITEMS)
+class HexdumpByteArrayHolder(val maxSize: Int = 16) {
+    private val bytes: ByteArray = ByteArray(maxSize)
     private var elements = 0
 
     fun size(): Int {
         return elements
     }
 
+    fun isFull(): Boolean {
+        return elements == maxSize
+    }
+
     fun push(b: Byte) {
-        if (elements >= MAX_ITEMS) throw IndexOutOfBoundsException("full")
+        if (elements >= maxSize) throw IndexOutOfBoundsException("full")
         bytes[elements++] = b
     }
 
     fun toHex(): String = buildString {
-        repeat(MAX_ITEMS) {
+        repeat(maxSize) {
             if (it >= elements) append("  ")
             else append("%02x".format(bytes[it]))
-            if (it < MAX_ITEMS - 1) append(" ")
+            if (it < maxSize - 1) append(" ")
         }
     }
 
     fun toAscii(): String = buildString {
-        repeat(MAX_ITEMS) {
+        repeat(maxSize) {
             if (it >= elements) append(" ")
             else append(Char(bytes[it].toUShort()).toString().replace(Regex("\\p{C}"), "."))
         }
@@ -37,7 +38,7 @@ class HexdumpByteArrayHolder {
     }
 }
 
-class HexdumpCollection {
+class HexdumpCollection(val maxSize: Int = 16) {
     private val collection = mutableListOf<HexdumpByteArrayHolder>()
 
     fun pushBytes(ba: ByteArray, forceNew: Boolean = false) {
@@ -50,10 +51,10 @@ class HexdumpCollection {
 
     fun pushByte(b: Byte, forceNew: Boolean = false) {
         if (collection.isEmpty() || forceNew) {
-            collection.add(HexdumpByteArrayHolder())
+            collection.add(HexdumpByteArrayHolder(maxSize))
         }
-        if (collection.last().size() == HexdumpByteArrayHolder.MAX_ITEMS) {
-            collection.add(HexdumpByteArrayHolder())
+        if (collection.last().isFull()) {
+            collection.add(HexdumpByteArrayHolder(maxSize))
         }
         collection.last().push(b)
     }
@@ -63,7 +64,7 @@ class HexdumpCollection {
     }
 }
 
-class DirectedHexdumpCollection {
+class DirectedHexdumpCollection(val maxSize: Int = 16) {
     enum class Direction {
         IN, OUT;
         override fun toString(): String = when(this) {
@@ -83,10 +84,10 @@ class DirectedHexdumpCollection {
 
     fun pushByte(dir: Direction, b: Byte, forceNew: Boolean = false) {
         if (collection.isEmpty() || forceNew) {
-            collection.add(Pair(dir, HexdumpCollection()))
+            collection.add(Pair(dir, HexdumpCollection(maxSize)))
         }
         if (collection.last().first != dir) {
-            collection.add(Pair(dir, HexdumpCollection()))
+            collection.add(Pair(dir, HexdumpCollection(maxSize)))
         }
         collection.last().second.pushByte(b)
     }
