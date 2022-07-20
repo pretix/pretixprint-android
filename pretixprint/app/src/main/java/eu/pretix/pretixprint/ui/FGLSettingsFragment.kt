9097e91cfafabda39756fd9390cfe14b85be61b2
Raphael Michel <michel@rami.io>
2022-07-20 18:24:43 +0200

    FGL: Support for two-path Lemur-2

--- pretixprint/app/src/main/java/eu/pretix/pretixprint/ui/FGLSettingsFragment.kt
+++ pretixprint/app/src/main/java/eu/pretix/pretixprint/ui/FGLSettingsFragment.kt
@@ -5,8 +5,11 @@ import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import eu.pretix.pretixprint.R
 import eu.pretix.pretixprint.byteprotocols.FGL
 import org.jetbrains.anko.support.v4.defaultSharedPreferences
@@ -20,6 +23,20 @@ class FGLSettingsFragment : SetupFragment() {
     ): View {
         val view = inflater.inflate(R.layout.fragment_fgl_settings, container, false)
         val proto = FGL()
+
+        val pathAdapter = ArrayAdapter(requireContext(), R.layout.list_item, FGL.Ticketpath.values().map {
+            it.id.toString()
+        })
+        (view.findViewById<TextInputLayout>(R.id.tilPath).editText as? AutoCompleteTextView)?.setAdapter(pathAdapter)
+
+        val chosenPathId = ((activity as PrinterSetupActivity).settingsStagingArea.get(
+                "hardware_${useCase}printer_path"
+        )) ?: defaultSharedPreferences.getString("hardware_${useCase}printer_path", "1")
+        if (chosenPathId?.isNotEmpty() == true) {
+            val chosenPath = FGL.Ticketpath.values().find { it.id.toString() == chosenPathId }!!.id.toString()
+            (view.findViewById<TextInputLayout>(R.id.tilPath).editText as? AutoCompleteTextView)?.setText(chosenPath, false)
+        }
+
         val currentDPI = ((activity as PrinterSetupActivity).settingsStagingArea.get(
                 "hardware_${useCase}printer_dpi"
         ) as String?) ?: defaultSharedPreferences.getString("hardware_${useCase}printer_dpi", proto.defaultDPI.toString())
@@ -29,15 +46,22 @@ class FGLSettingsFragment : SetupFragment() {
             back()
         }
         view.findViewById<Button>(R.id.btnNext).setOnClickListener {
+            val path = view.findViewById<TextInputLayout>(R.id.tilPath).editText?.text.toString()
             val dpi = view.findViewById<TextInputEditText>(R.id.teDPI).text.toString()
             if (TextUtils.isEmpty(dpi)) {
                 view.findViewById<TextInputEditText>(R.id.teDPI).error = getString(R.string.err_field_required)
             } else if (!TextUtils.isDigitsOnly(dpi)) {
                 view.findViewById<TextInputEditText>(R.id.teDPI).error = getString(R.string.err_field_invalid)
+            } else if (TextUtils.isEmpty(path)) {
+                view.findViewById<TextInputEditText>(R.id.tilPath).error = getString(R.string.err_field_required)
+            } else if (!TextUtils.isDigitsOnly(path)) {
+                view.findViewById<TextInputEditText>(R.id.tilPath).error = getString(R.string.err_field_invalid)
             } else {
                 view.findViewById<TextInputEditText>(R.id.teDPI).error = null
                 (activity as PrinterSetupActivity).settingsStagingArea.put("hardware_${useCase}printer_dpi",
                         dpi)
+                (activity as PrinterSetupActivity).settingsStagingArea.put("hardware_${useCase}printer_path",
+                        path)
                 (activity as PrinterSetupActivity).startFinalPage()
             }
         }
