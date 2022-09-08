2472bd8d60bab8533162b07911a5c30d175dbd79
Maximilian Richt <richt@rami.io>
2022-09-08 12:53:37 +0200

    Remove stetho

--- pretixprint/app/src/main/java/eu/pretix/pretixprint/PretixPrint.kt
+++ pretixprint/app/src/main/java/eu/pretix/pretixprint/PretixPrint.kt
@@ -1,7 +1,6 @@
 package eu.pretix.pretixprint
 
 import androidx.multidex.MultiDexApplication
-import com.facebook.stetho.Stetho
 import com.tom_roush.pdfbox.util.PDFBoxResourceLoader
 import eu.pretix.pretixprint.print.WYSIWYGRenderer
 
@@ -9,10 +8,7 @@ class PretixPrint : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
 
-        if (BuildConfig.DEBUG) {
-            Stetho.initializeWithDefaults(this)
-        }
         WYSIWYGRenderer.registerFonts(this)
-        PDFBoxResourceLoader.init(getApplicationContext());
+        PDFBoxResourceLoader.init(getApplicationContext())
     }
 }
