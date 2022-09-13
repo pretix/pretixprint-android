40512088fe89ea57e16d8824100dc00d052a8422
Raphael Michel <michel@rami.io>
2022-09-13 10:10:14 +0200

    StarPRINT should not be available for sunmi internal connection

--- pretixprint/app/src/main/java/eu/pretix/pretixprint/byteprotocols/StarPRNT.kt
+++ pretixprint/app/src/main/java/eu/pretix/pretixprint/byteprotocols/StarPRNT.kt
@@ -1,6 +1,10 @@
 package eu.pretix.pretixprint.byteprotocols
 
+import com.zebra.sdk.comm.BluetoothConnection
 import eu.pretix.pretixprint.R
+import eu.pretix.pretixprint.connections.ConnectionType
+import eu.pretix.pretixprint.connections.NetworkConnection
+import eu.pretix.pretixprint.connections.USBConnection
 import eu.pretix.pretixprint.ui.SetupFragment
 
 class StarPRNT : ESCPOS() {
@@ -9,5 +13,9 @@ class StarPRNT : ESCPOS() {
 
     override fun createSettingsFragment(): SetupFragment? {
         return null
+    }
+
+    override fun allowedForConnection(type: ConnectionType): Boolean {
+        return type is USBConnection || type is NetworkConnection || type is BluetoothConnection
     }
 }
