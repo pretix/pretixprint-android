2d02930f396eb2e0ca3acf1e58edf3ff13e5d483
Maximilian Richt <richt@rami.io>
2023-03-06 14:23:22 +0100

    Add flipper for ui, settings and network debugging

---
+++ pretixprint/app/src/main/java/eu/pretix/pretixprint/FlipperInitializer.kt
@@ -0,0 +1,25 @@
+package eu.pretix.pretixprint
+
+import android.app.Application
+import android.content.Context
+import com.facebook.flipper.core.FlipperClient
+import com.facebook.flipper.plugins.crashreporter.CrashReporterPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
+import com.facebook.flipper.plugins.uidebugger.UIDebuggerFlipperPlugin
+import com.facebook.flipper.plugins.uidebugger.core.UIDContext
+
+
+object FlipperInitializer {
+    fun initFlipperPlugins(context: Context, client: FlipperClient) {
+        val descriptorMapping = DescriptorMapping.withDefaults()
+        client.addPlugin(InspectorFlipperPlugin(context, descriptorMapping))
+        client.addPlugin(UIDebuggerFlipperPlugin(UIDContext.create(context as Application)))
+        client.addPlugin(NetworkFlipperPlugin())
+        client.addPlugin(CrashReporterPlugin.getInstance())
+        client.addPlugin(SharedPreferencesFlipperPlugin(context))
+        client.start()
+    }
+}
--- pretixprint/app/src/main/java/eu/pretix/pretixprint/PretixPrint.kt
+++ pretixprint/app/src/main/java/eu/pretix/pretixprint/PretixPrint.kt
@@ -1,6 +1,10 @@
 package eu.pretix.pretixprint
 
 import androidx.multidex.MultiDexApplication
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.core.FlipperClient
+import com.facebook.soloader.SoLoader
 import com.tom_roush.pdfbox.util.PDFBoxResourceLoader
 import eu.pretix.pretixprint.print.WYSIWYGRenderer
 import org.jetbrains.anko.defaultSharedPreferences
@@ -11,6 +15,13 @@ class PretixPrint : MultiDexApplication() {
 
         WYSIWYGRenderer.registerFonts(this)
         PDFBoxResourceLoader.init(getApplicationContext())
+
+        SoLoader.init(this, false)
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client: FlipperClient = AndroidFlipperClient.getInstance(this)
+            FlipperInitializer.initFlipperPlugins(this, client)
+        }
+
         migrateSettings()
     }
 
