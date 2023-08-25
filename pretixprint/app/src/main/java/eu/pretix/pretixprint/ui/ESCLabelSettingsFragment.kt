eb839459600fc45bfc4be5edbaa204c2e1385c80
Martin Gross <gross@rami.io>
2023-08-25 09:04:40 +0200

    Support for ESC/Label color label printers (#59)

---
+++ pretixprint/app/src/main/java/eu/pretix/pretixprint/ui/ESCLabelSettingsFragment.kt
@@ -0,0 +1,72 @@
+package eu.pretix.pretixprint.ui
+
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import androidx.preference.PreferenceManager
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import eu.pretix.pretixprint.R
+import eu.pretix.pretixprint.Rotation
+import eu.pretix.pretixprint.byteprotocols.SLCS
+
+class ESCLabelSettingsFragment : SetupFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        val view = inflater.inflate(R.layout.fragment_fgl_settings, container, false)
+        val proto = SLCS()
+
+        val currentDPI = ((activity as PrinterSetupActivity).settingsStagingArea.get(
+            "hardware_${useCase}printer_dpi"
+        ) as String?) ?: prefs.getString("hardware_${useCase}printer_dpi", proto.defaultDPI.toString())
+        view.findViewById<TextInputEditText>(R.id.teDPI).setText(currentDPI)
+
+        val rotationAdapter = ArrayAdapter(requireContext(), R.layout.list_item, Rotation.values().map {
+            it.toString()
+        })
+        (view.findViewById<TextInputLayout>(R.id.tilRotation).editText as? AutoCompleteTextView)?.setAdapter(rotationAdapter)
+        val chosenRotation = ((activity as PrinterSetupActivity).settingsStagingArea.get(
+            "hardware_${useCase}printer_rotation"
+        )) ?: prefs.getString("hardware_${useCase}printer_rotation", "0")
+        if (chosenRotation?.isNotEmpty() == true) {
+            val chosenLabel = Rotation.values().find { it.degrees == Integer.valueOf(chosenRotation) }!!.toString()
+            (view.findViewById<TextInputLayout>(R.id.tilRotation).editText as? AutoCompleteTextView)?.setText(chosenLabel, false)
+        }
+
+        view.findViewById<Button>(R.id.btnPrev).setOnClickListener {
+            back()
+        }
+        view.findViewById<Button>(R.id.btnNext).setOnClickListener {
+            val dpi = view.findViewById<TextInputEditText>(R.id.teDPI).text.toString()
+            val rotation = view.findViewById<TextInputLayout>(R.id.tilRotation).editText?.text.toString()
+            if (TextUtils.isEmpty(dpi)) {
+                view.findViewById<TextInputEditText>(R.id.teDPI).error = getString(R.string.err_field_required)
+            } else if (!TextUtils.isDigitsOnly(dpi)) {
+                view.findViewById<TextInputEditText>(R.id.teDPI).error = getString(R.string.err_field_invalid)
+            } else {
+                view.findViewById<TextInputEditText>(R.id.teDPI).error = null
+                val mappedRotation = Rotation.values().find { it.toString() == rotation }!!.degrees
+
+                (activity as PrinterSetupActivity).settingsStagingArea.put("hardware_${useCase}printer_rotation", mappedRotation.toString())
+                (activity as PrinterSetupActivity).settingsStagingArea.put("hardware_${useCase}printer_dpi",
+                    dpi)
+                (activity as PrinterSetupActivity).startFinalPage()
+            }
+        }
+
+        return view
+    }
+
+    override fun back() {
+        (activity as PrinterSetupActivity).startProtocolChoice()
+    }
+}
