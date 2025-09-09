2a1cd53b346c3a15785108f1d769615f2d127177
martinrichter <richter@pretix.eu>
2025-09-09 13:19:06 +0200

    Changed Keyboardtype in Pindialog to nummeric

--- pretixprint/app/src/main/java/eu/pretix/pretixprint/ui/ProtectedEditTextPreference.kt
+++ pretixprint/app/src/main/java/eu/pretix/pretixprint/ui/ProtectedEditTextPreference.kt
@@ -1,6 +1,7 @@
 package eu.pretix.pretixprint.ui
 
 import android.content.Context
+import android.text.InputType
 import android.util.AttributeSet
 import androidx.preference.EditTextPreference
 
@@ -8,6 +9,13 @@ class ProtectedEditTextPreference(context: Context, attrs: AttributeSet) :
     EditTextPreference(context, attrs) {
 
     var earlyClickListener: OnPreferenceClickListener? = null
+
+    init {
+        // Input Type für numerische PIN-Eingabe setzen
+        setOnBindEditTextListener { editText ->
+            editText.inputType = InputType.TYPE_CLASS_NUMBER
+        }
+    }
 
     fun setEarlyPreferenceClickListener(onPreferenceClickListener: OnPreferenceClickListener) {
         earlyClickListener = onPreferenceClickListener
